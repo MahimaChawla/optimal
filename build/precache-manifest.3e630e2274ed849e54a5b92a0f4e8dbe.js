a9014bff0c70297f6745b172d2817401b9f19574
Mahima Chawla <mahima.chawla@classpass.com>
2020-06-11 18:42:59 -0600

    Initial commit

---
+++ build/precache-manifest.3e630e2274ed849e54a5b92a0f4e8dbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a161dbc9aeee32c99beb97ad56e25e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4e1a4fbd1ea187dc953",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe6c96694ddcf4745973",
+    "url": "/static/js/2.024a7ad3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.024a7ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e1a4fbd1ea187dc953",
+    "url": "/static/js/main.798aa112.chunk.js"
+  },
+  {
+    "revision": "c7dc250321327eefe8af",
+    "url": "/static/js/runtime-main.a91beaab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
